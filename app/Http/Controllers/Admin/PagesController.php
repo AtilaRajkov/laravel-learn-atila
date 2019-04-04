bf4d7d67a65dab4cea52a651af22eeb891f8fe82
atila <atila@pista-Inspiron-3542>
2019-04-04 17:02:53 +0200

    Zavrsen 'store'

--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Page;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class PagesController extends Controller {
-   
+
    public function __construct() {
-        $this->middleware('auth');
-        $this->middleware('isadmin')->only(['create', 'store']);
-    }
+      $this->middleware('auth');
+      $this->middleware('isadmin')->only(['create', 'store']);
+   }
 
    /**
     * Display a listing of the resource.
@@ -19,7 +21,9 @@ class PagesController extends Controller {
     * @return \Illuminate\Http\Response
     */
    public function index() {
-      //
+      $rows = Page::all();
+
+      return $rows;
    }
 
    /**
@@ -39,56 +43,72 @@ class PagesController extends Controller {
     */
    public function store(Request $request) {
       // Validacija:
-      request()->validate([
-          'title' => 'required|string|min:3|max:191',
+      $data = request()->validate([
+          'title' => 'required|string|min:3|max:191|unique:pages',
           'description' => 'required|string|min:3|max:191',
-          'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+          'image' => 'required|image|mimes:jpeg,png,jpg,bmp|max:2048',
           'content' => 'required|string|min:3|max:65000',
           'layout' => 'required|string|in:fullwidth,leftaside,rightaside',
           'contact_form' => 'required|in:0,1',
-          'header' => 'required|in:0,1',
-          'aside' => 'required|in:0,1',
-          'footer' => 'required|in:0,1',
-          'active' => 'required|in:0,1',
+          'header' => 'required|boolean',
+          'aside' => 'required|boolean',
+          'footer' => 'required|boolean',
+          'active' => 'required|boolean',
       ]);
-      
-      $newPage = new Page();
-      $newPage->title = request('title');
-      $newPage->description = request('description');
-      
-      $newPage->content = request('content');
-      $newPage->layout = request('layout');
-      $newPage->contact_form = request('contact_form');
-      $newPage->header = request('header');
-      $newPage->aside = request('aside');
-      $newPage->footer = request('footer');
-      $newPage->active = request('active');
 
-      if ($request->hasFile('image')) {
+      $row = new Page();
+
+      unset($data['image']);
+      foreach ($data as $key => $value) {
+         $row->$key = $value;
+      }
+      $row->image = '';
+      // provera da li uopste dolazi 'image' kroz request
+      if (request()->has('image')) {
+         $file = request()->image;
+         $fileExtension = $file->getClientOriginalExtension();
+
+         $fileName = $file->getClientOriginalName();
+         $fileName = pathinfo($fileName, PATHINFO_FILENAME);
+         $fileName = config('app.seo-image-prefiks') . Str::slug(request('title'), '-') . '-' . Str::slug(now(), '-') . '.' . $fileExtension;
+
+//         echo "<br>" . public_path('/upload/pages/');
+         $file->move(public_path('/upload/pages/'), $fileName);
+
+         $row->image = '/upload/pages/' . $fileName;
+
+         // Intervention paket
+         // xl velicicina
+         $interventionImage = Image::make(public_path('/upload/pages/') . $fileName);
+         $interventionImage->resize(1140, null, function ($constraint) {
+            $constraint->aspectRatio();
+         });
+         $fileNameXL = '/upload/pages/' . config('app.seo-image-prefiks') . Str::slug(request('title'), '-') . '-' . Str::slug(now(), '-') . '-XL.' . $fileExtension;
+         $interventionImage->save(public_path($fileNameXL));
          
-         $storage_folder = '/images';
+         // m velicicina
+         $interventionImage = Image::make(public_path('/upload/pages/') . $fileName);
+         $interventionImage->resize(800, null, function ($constraint) {
+            $constraint->aspectRatio();
+         });
+         $fileNameM = '/upload/pages/' . config('app.seo-image-prefiks') . Str::slug(request('title'), '-') . '-' . Str::slug(now(), '-') . '-m.' . $fileExtension;
+         $interventionImage->save(public_path($fileNameM));
          
-         $image = $request->file('image');
-         $name = time() . '.' . $image->getClientOriginalExtension();
-         $destinationPath = public_path($storage_folder);
-         $image->move($destinationPath, $name);
-         
-         $newPage->image = $storage_folder . '/' . $name;
-         
-         $newPage->save();
-         
-         session()->flash('message-type', 'success');
-         session()->flash('message-text', 'Successfully created a new page!!!');
-         
-//         return redirect( route('pages.create') );
-         return back();
+         // s velicicina
+         $interventionImage = Image::make(public_path('/upload/pages/') . $fileName);
+         $interventionImage->resize(300, null, function ($constraint) {
+            $constraint->aspectRatio();
+         });
+         $fileNameS = '/upload/pages/' . config('app.seo-image-prefiks') . Str::slug(request('title'), '-') . '-' . Str::slug(now(), '-') . '-s.' . $fileExtension;
+         $interventionImage->save(public_path($fileNameS));
       }
 
-      session()->flash('message-type', 'danger');
-      session()->flash('message-text', 'An error occurred!');
+      $row->save();
 
-      return back();
-      //return redirect()->route('pages.index');
+      session()->flash('message-type', 'success');
+      session()->flash('message-text', 'Successfully created page' . $row->title . '!');
+
+      return redirect()->route('pages.index');
    }
 
    /**
--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -35,7 +35,7 @@
                     <!--DESCRIPTION-->
                     <div class="form-group">
                         <label>Description *</label>
-                        <textarea name="description" class="form-control">{{ old("description") }}</textarea>
+                        <textarea name="description" class="form-control">{!! old("description") !!}</textarea>
                         @if($errors->has('description'))
                         <div class='text text-danger'>
                             {{ $errors->first('description') }}
@@ -58,119 +58,165 @@
                     <!--CONTENT-->
                     <div class="form-group">
                         <label>Content *</label>
-                        <textarea id="content-create" name="content" class="form-control">{{ old("content") }}</textarea>
+                        <textarea id="content-create" name="content" class="form-control">{!! old("content") !!}</textarea>
                         @if($errors->has('content'))
                         <div class='text text-danger'>
                             {{ $errors->first('content') }}
                         </div>
                         @endif
                     </div>
-                    
+
                     <!--LAYOUT-->
                     <div class="form-group">
-                        <label>Layout</label>
-                        <select name="layout" class="form-control">
-                            <option value=''>-- Choose page layout --</option>
-                            <option value='fullwidth' {{ (old('layout') == 'fullwidth') ? 'selected' : '' }}>Width 100%</option>
-                            <option value='leftaside' {{ (old('layout') == 'leftaside') ? 'selected' : '' }}>With left aside</option>
-                            <option value='rightaside' {{ (old('layout') == 'rightaside') ? 'selected' : '' }}>With right aside</option>
-                        </select>
+                        <div class="row">
+                            <label class="col-form-label col-sm-2 pt-2">Layout</label>
+                            <div class="col-sm-3">
+                                <select name="layout" class="form-control">
+                                    <option value=''>-- Choose page layout --</option>
+                                    <option value='fullwidth' {{ (old('layout') == 'fullwidth') ? 'selected' : '' }}>Width 100%</option>
+                                    <option value='leftaside' {{ (old('layout') == 'leftaside') ? 'selected' : '' }}>With left aside</option>
+                                    <option value='rightaside' {{ (old('layout') == 'rightaside') ? 'selected' : '' }}>With right aside</option>
+                                </select>
+                            </div>
+                        </div>
                         @if($errors->has('layout'))
                         <div class='text text-danger'>
                             {{ $errors->first('layout') }}
                         </div>
                         @endif
                     </div>
+                    <!--CONTACT FORM -->
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Contact form?</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="contact_form" id="gridRadios1" value="1" {{ (old('contact_form', 0)) == 1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios1">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="contact_form" id="gridRadios2" value="0" {{ (old('contact_form', 0)) == 0 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios2">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                            @if($errors->has('contact_form'))
+                            <div class='text text-danger'>
+                                {{ $errors->first('contact_form') }}
+                            </div>
+                            @endif
+                        </div>
+                    </fieldset>
 
-                    <!--MOJ CONTACT FORM (inline pokusaj 1) -->
-                    <div class="form-group">
-                        <label>Contact form</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="contact_form" id="contact-form-radio-1" value="0" {{ (old('layout', 0)) == 0 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="contact-form-radio-1">No</label>
+                    <!--HEADER MENU-->
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Header menu?</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="header" id="gridRadios1-header" value="1" {{ (old('header', 1)) == 1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios1-header">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="header" id="gridRadios2-header" value="0" {{ (old('header', 1)) == 0 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios2-header">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                            @if($errors->has('header'))
+                            <div class='text text-danger'>
+                                {{ $errors->first('header') }}
+                            </div>
+                            @endif
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="contact_form" id="contact-form-radio-2" value="1" {{ (old('layout')) == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="contact-form-radio-2">Yes</label>
+                    </fieldset>
+
+
+
+                    <!--ASIDE-->
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Aside menu?</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="aside" id="gridRadios1aside" value="1" {{ (old('aside', 1)) == 1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios1aside">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="aside" id="gridRadios2aside" value="0" {{ (old('aside', 1)) == 0 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios2aside">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                            @if($errors->has('aside'))
+                            <div class='text text-danger'>
+                                {{ $errors->first('aside') }}
+                            </div>
+                            @endif
                         </div>
-                        @if($errors->has('contact_form'))
-                        <div class='text text-danger'>
-                            {{ $errors->first('contact_form') }}
-                        </div>
-                        @endif
-                    </div>
-                    
-                    <!--HEADER -->
-                    <div class="form-group">
-                        <label class="mr-5">Header</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="header" id="header-radio-1" value="0" {{ (old('header')) == 0 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="header-radio-1">No</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="header" id="header-radio-2" value="1" {{ (old('header', 1)) == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="header-radio-2">Yes</label>
-                        </div>
-                        @if($errors->has('header'))
-                        <div class='text text-danger'>
-                            {{ $errors->first('header') }}
-                        </div>
-                        @endif
-                    </div>
-                    
-                    <!--ASIDE -->
-                    <div class="form-group">
-                        <label class="mr-5">Aside</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="aside" id="aside-radio-1" value="0" {{ (old('aside')) == 0 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="aside-radio-1">No</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="aside" id="aside-radio-2" value="1" {{ (old('aside', 1)) == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="aside-radio-2">Yes</label>
-                        </div>
-                        @if($errors->has('aside'))
-                        <div class='text text-danger'>
-                            {{ $errors->first('aside') }}
-                        </div>
-                        @endif
-                    </div>
-                    
+                    </fieldset>
+
                     <!--FOOTER -->
-                    <div class="form-group">
-                        <label class="mr-5">Footer</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="footer" id="footer-radio-1" value="0" {{ (old('footer', 0)) == 0 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="footer-radio-1">No</label>
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Footer menu?</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="footer" id="gridRadios1footer" value="1" {{ (old('footer', 0)) == 1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios1footer">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="footer" id="gridRadios2footer" value="0" {{ (old('footer', 0)) == 0 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios2footer">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                            @if($errors->has('footer'))
+                            <div class='text text-danger'>
+                                {{ $errors->first('footer') }}
+                            </div>
+                            @endif
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="footer" id="footer-radio-2" value="1" {{ (old('footer')) == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="footer-radio-2">Yes</label>
+                    </fieldset>
+
+                    <!--CONTACT FORM -->
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Page is active?</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="active" id="gridRadios1active" value="1" {{ (old('active', 0)) == 1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios1active">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="active" id="gridRadios2active" value="0" {{ (old('active', 0)) == 0 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="gridRadios2active">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
+                            @if($errors->has('active'))
+                            <div class='text text-danger'>
+                                {{ $errors->first('active') }}
+                            </div>
+                            @endif
                         </div>
-                        @if($errors->has('footer'))
-                        <div class='text text-danger'>
-                            {{ $errors->first('footer') }}
-                        </div>
-                        @endif
-                    </div>
-                    
-                    <!--ACTIVE -->
-                    <div class="form-group">
-                        <label class="mr-5">Active</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="active" id="active-radio-1" value="0" {{ (old('active', 0)) == 0 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="active-radio-1">No</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="active" id="active-radio-2" value="1" {{ (old('active')) == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="active-radio-2">Yes</label>
-                        </div>
-                        @if($errors->has('active'))
-                        <div class='text text-danger'>
-                            {{ $errors->first('active') }}
-                        </div>
-                        @endif
-                    </div>
+                    </fieldset>
+
 
                     <div class="form-group text-right">
                         <button type='submit' class="btn btn-primary">Save</button>
